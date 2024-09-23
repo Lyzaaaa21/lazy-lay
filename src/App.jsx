c53b7a38df4a42975713d4ace73d4468a77ce8f0
Lyzaaaa21 <lauzonlyza12@gmail.com>
2024-09-23 20:01:45 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Navbar from "./components/Navbar";
+
+function App() {
+
+  return (
+    <>
+          {/* <h1 className="text-3xl font-bold text-brandPrimary"> 
+            Hello world!
+          </h1> */}
+
+          <Navbar />
+    </>
+  );
+
+}
+
+export default App;
